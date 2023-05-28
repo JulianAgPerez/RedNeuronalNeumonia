f9e735e18f1251af1a92265cba0035f07cdc42d0
Abri-Sabatini <lucina.sabatini@alumnos.frm.utn.edu.ar>
2023-05-28 20:38:33 -0300

    Add files via upload
    Interfaz gráfica.  V0.3

--- interfaz.py
+++ interfaz.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import ImageTk, Image
 
-#interfaz gráfica.  V0.2
+#interfaz gráfica.  V0.3
 
 """
 FALTA AGREGAR:
@@ -13,28 +13,32 @@ FALTA AGREGAR:
 root = Tk()
 
 root.title('PulmonIA')
-root.geometry("700x500")
-root.configure(bg= "powder blue")
+root.configure(bg= "deep sky blue")
 
-                                                #CENTRALIZADO DE VENTANA
+
+#CENTRALIZADO DE VENTANA
 # Obtiene el ancho y la altura de la pantalla
 ancho_pantalla = root.winfo_screenwidth()
 altura_pantalla = root.winfo_screenheight()
 #Ajusta el ancho y alto de la ventana
-ancho_ventana = 600
-altura_ventana = 400
+ancho_ventana = 800
+altura_ventana = 550
 # Calcula las coordenadas x e y para centrar la ventana
 x = (ancho_pantalla - ancho_ventana) // 2
 y = (altura_pantalla - altura_ventana) // 2
 root.geometry(f"{ancho_ventana}x{altura_ventana}+{x}+{y}")
 
-Label(root, text="Radiografía").pack
+Label(root, text="RADIOGRAFÍA DE TÓRAX", pady=10, bg="deep sky blue", font=("Ebrima", 12, "bold")).pack(pady=5)
 
-mostrar_image = Label(root, bg="powder blue")
-mostrar_image.pack(pady=20)
+mostrar_image = Label(root, bg="deep sky blue")
+mostrar_image.pack()
 
 #150 x 150 imagenes que recibe la red 
- 
+
+# Cuadro blanco para mostrar la imagen
+#cuadro_blanco = Label(root, bg="white", width=30, height=18)
+#cuadro_blanco.pack()
+
 # Función para cargar y analizar la imagen seleccionada
 def agregar_imagen():
    imagen = filedialog.askopenfilename()
@@ -42,19 +46,22 @@ def agregar_imagen():
    image_tk = ImageTk.PhotoImage(image)
    mostrar_image.configure(image=image_tk)
    mostrar_image.image = image_tk
+   #cuadro_blanco.destroy()
    btn_agregar_imagen.destroy()  # Eliminar el botón después de utilizarlo
+  
    
 # Función para salir de la aplicación
 def salir():
     root.destroy()   
     
+    
 #botones 
 btn_agregar_imagen = Button(root, text="Agregar imagen", command=agregar_imagen, width=17)
-btn_agregar_imagen.configure(font=("Ebrima", 10))
-btn_agregar_imagen.pack(pady=20) #Margen
+btn_agregar_imagen.configure(font=("Ebrima", 11, "bold"))
+btn_agregar_imagen.pack(pady=150) #Margen
 
 btn_salir = Button(root, text="Salir", command=salir, width=10)
-btn_salir.configure(font=("Ebrima", 10))
-btn_salir.pack(side="bottom", anchor="se", pady=17, padx=20) #Margen 
+btn_salir.configure(font=("Ebrima", 11, "bold"))
+btn_salir.pack(side="bottom", anchor="se", pady=5, padx=20) #Margen 
 
 root.mainloop()
